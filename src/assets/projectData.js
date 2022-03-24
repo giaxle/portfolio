a3a7bcf8dc33a91442cc10912508ffd526e0f7bc
Giaxle <giaxle@outlook.com>
2022-03-24 16:06:14 -0400

    refactor and styling

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -1,8 +1,7 @@
 // ---------Project Images----------
-import todolist from "./images/todolist.png";
 import portfolio from "./images/portfolio.png";
-import country from "./images/countryinfoapp.png";
 import penguin from "./images/penguin.png";
+import cv from "./images/cv-sandbox.png";
 // ---------Project Images----------
 
 // -------- Icons -----------
@@ -12,8 +11,6 @@ import html from "../assets/icons/html5-original.svg";
 import css from "../assets/icons/css3-original.svg";
 import node from "../assets/icons/nodejs-original.svg";
 import sass from "../assets/icons/sass-original.svg";
-import linux from "../assets/icons/linux-original.svg";
-import npm from "../assets/icons/npm-original-wordmark.svg";
 import mongo from "../assets/icons/mongodb.svg";
 // -------- Icons -----------
 // tech: [react, mongo, css, node],
@@ -32,30 +29,30 @@ const data = [
     img: penguin,
   },
   {
-    name: "CV Sandbox",
-    tech: [react, mongo, html, css, node],
+    name: "CV - Sandbox",
+    tech: [react, mongo, html, sass, node],
 
     about: [
       "- Full-stack resume generator",
       "- Stores user progress in MongoDB database",
       "- Generates PDF when finished",
     ],
-    live: "https://mern-penguin.herokuapp.com/",
-    repo: "https://github.com/giaxle/penguin",
-    img: penguin,
+    live: "https://mern-cv-sandbox.herokuapp.com/",
+    repo: "https://github.com/giaxle/cv-sandbox",
+    img: cv,
   },
   {
-    name: "MyWeatherNow",
-    tech: [react, html, css, js],
+    name: "https://giahuyle.com/",
+    tech: [react, html, css, sass, js],
 
     about: [
       "- React SPA",
-      "- Fetches weather data from third-party API",
-      "- Displays 7-day forecast",
+      "- My personal website to showcase my work",
+      "- You're already here. Take a look around!",
     ],
-    live: "https://mern-penguin.herokuapp.com/",
-    repo: "https://github.com/giaxle/penguin",
-    img: penguin,
+    live: "https://giahuyle.com/",
+    repo: "https://github.com/giaxle/portfolio",
+    img: portfolio,
   },
 ];
 
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,15 +14,16 @@ const About = () => {
 
         <div className={style.about}>
           <div className={style.aboutMeContent}>
-            <img className={style.avatar} src={headshot} />
+            <img className={style.avatar} src={headshot} alt="headshot" />
             <div>
-              I'm <strong>Gia Huy Le</strong>! I'm a music teacher and
-              full-stack developer with a thirst for knowledge. I'm dedicated to
-              the life-long process of learning. As I've ascended the cliffs of
-              software development, I realized the end never comes into sight.
-              But looking back, I see where I started and the height I've
-              climbed. Software development started off as a hobby, but turned
-              into a lifestyle.
+              I'm <strong>Gia Huy Le</strong>! I'm a music teacher/web developer
+              with a thirst for knowledge and a dedication to learning. I
+              started my web development journey with theOdinProject and
+              freeCodeCamp. As I've ascended the cliffs of software development,
+              I realized the end never comes into sight. But looking back, I see
+              where I started and the height I've climbed. It's a fullfilling
+              journey and I look forward to experiencing more. Software
+              development started off as a hobby, but turned into a lifestyle.
               <br />
               <br />
               Personal Hobbies: Mountain biking, weight lifting, tinkering with
